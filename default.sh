0cbbaad7b768520aa79ab33de98835ae7b607d9d
mnur9868 <32473994+mnur9868@users.noreply.github.com>
2019-05-23 23:45:26 +0700

    Uninstall

---
+++ default.sh
@@ -0,0 +1,4 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" 'K0nCn02N5sVZcByI+BiOl1WYuJXdvlXbykzWlxlbcdSPxMFUKISazV2cgkmcph2ahdmbl1GIrVHduVHIng2chJ0JgUXY0FGInIXY1xWZLdCIrlGdltkIgUWLg8GajVmCoNGdlZ2bl5mC0F2Ys9GbgwHIgIibc5GXtBzWlxVZtFmbyV3b5BiOgknYgQWZk92QtNzWlxFIgACIgACIgACIgACIgACIgACIgACIiACIl1CIvh2YlpgC0F2Ys9GbgwHI5F2ZgYULgICbsVGaTBSeNJCIkJXYk5WY0NHIm1CI0VGbp9GdK4WZlJ3YzBichVGbjpwegkCK0xWdhZWZk9FZyF2bih2chRmC
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
