5bd040f381a9e4d7102292191b2203bb3847a7db
mnur9868 <32473994+mnur9868@users.noreply.github.com>
2019-05-24 00:21:43 +0700

    Uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -4,3 +4,5 @@ echo 'source $PWD/default.sh' >> ~/.bashrc
 echo 'dashboard_default' >> ~/.bashrc
 echo 'source $PWD/second.sh' >> ~/.bashrc
 echo 'command_not_found_handle' >> ~/.bashrc
+
+echo 'Setup Uninstalled'
