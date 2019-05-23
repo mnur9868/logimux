491823094d9251efe88f28b0c31c91a680d816f4
mnur9868 <32473994+mnur9868@users.noreply.github.com>
2019-05-24 00:12:38 +0700

    Fix issues current directory

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 > ~/.bashrc
 										
-echo 'source /data/data/com.termux/files/home/first/default.sh' >> ~/.bashrc
+echo 'source $PWD/default.sh' >> ~/.bashrc
 echo 'dashboard_default' >> ~/.bashrc
-echo 'source /data/data/com.termux/files/home/first/second.sh' >> ~/.bashrc
+echo 'source $PWD/second.sh' >> ~/.bashrc
 echo 'command_not_found_handle' >> ~/.bashrc
