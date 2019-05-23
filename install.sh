f43bc5de869c6dce60a2119c31ea274d131a32c5
mnur9868 <32473994+mnur9868@users.noreply.github.com>
2019-05-23 23:46:00 +0700

    Master

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/data/data/com.termux/files/usr/bin/bash
+> ~/.bashrc	
+          
+echo 'source /data/data/com.termux/files/home/first/logimux.sh' >> ~/.bashrc 
+
+echo 'source /data/data/com.termux/files/home/first/first_en.sh' >> ~/.bashrc
+
+echo 'LogiMux' >> ~/.bashrc
+echo 'command_not_found_handle' >> ~/.bashrc
