a7c2d18a7a699eb3d35dd7d9fe19ed659c70bbfa
mnur9868 <32473994+mnur9868@users.noreply.github.com>
2019-05-23 23:47:46 +0700

    Uninstall

---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+> ~/.bashrc
+										
+echo 'source /data/data/com.termux/files/home/first/default.sh' >> ~/.bashrc
+echo 'dashboard_default' >> ~/.bashrc
+echo 'source /data/data/com.termux/files/home/first/second.sh' >> ~/.bashrc
+echo 'command_not_found_handle' >> ~/.bashrc
