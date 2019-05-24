846becec41eec790e96c05945964bc7272c8d4a5
mnur9868 <32473994+mnur9868@users.noreply.github.com>
2019-05-24 15:56:28 +0700

    Update

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
 > ~/.bashrc	
           
-echo 'source $PWD/logimux.sh' >> ~/.bashrc 
+echo 'source $HOME/logimux/logimux.sh' >> ~/.bashrc 
 
-echo 'source $PWD/first_en.sh' >> ~/.bashrc
+echo 'source $HOME/logimux/first_en.sh' >> ~/.bashrc
 
 echo 'LogiMux' >> ~/.bashrc
 echo 'command_not_found_handle' >> ~/.bashrc
